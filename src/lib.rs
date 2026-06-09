dd252b9a39d78ca8ab98d4b446215bb5d447d8a4
AxelSeven <spaghettiandmeatballs61@gmail.com>
2026-06-09 05:22:31 +0200

    feat: Add standalone feature, to separate standalone and crate building

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use godot::prelude::*;
-
-
-struct BoundlessExtension;
-
-#[gdextension]
-unsafe impl ExtensionLibrary for BoundlessExtension {}
-
-
 pub mod framework;
+
+#[cfg(feature = "standalone")]
+mod entry {
+	use godot::prelude::*;
+
+	struct BoundlessExtension;
+
+	#[gdextension]
+	unsafe impl ExtensionLibrary for BoundlessExtension {}
+}
